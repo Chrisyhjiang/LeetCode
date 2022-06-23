eb4751bebf459ab3d4844a529caa33b92212ff07
wenbojiang <jiangwb70@yahoo.ca>
2022-06-23 16:41:33 -0400

    finished the modified binary search version of  search in rotated array

--- BinarySearch/SearchRotatedSortedArray.java
+++ BinarySearch/SearchRotatedSortedArray.java
@@ -76,9 +76,49 @@ public class SearchRotatedSortedArray {
 		return result;
 	}
 	
+	public static int search2(int[] nums, int target) {
+		// modified binary search
+		int start = 0;
+		int index = -1;
+		int end = nums.length - 1;
+		while(start <= end) {
+			// mid is either smaller than both start and end or bigger than both start and end. 
+			int mid = (start + end) / 2;
+			if(nums[mid] == target) {
+				index = mid;
+				break;
+			}
+			if (nums[mid] >= nums[start]) {
+				// from start to mid it is in ascending order. 
+				if(target < nums[mid] && target >= nums[start]) {
+					// element is in between pivot and the start so shorten end. 
+					end = mid - 1;
+				}else {
+					// element is from mid to end as it is either larger than mid or smaller than start. 
+					start = mid + 1;
+				}
+			}else {
+				// from mid to end is ascending order. 
+				if(target <= nums[end] && target > nums[mid]) {
+					// if the target is bigger than mid but smaller than end
+					// search between mid and end shift start
+					start = mid + 1;
+				}else {
+					// else element is before mid as it is either smaller than mid or larger than end. 
+					end = mid -1;
+				}
+			}
+			
+		}
+		return index;
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		int[] nums = {1,3};
-		int k = search(nums, 3);
+		int k = search2(nums, 3);
 		System.out.println(k);
 	}
 
