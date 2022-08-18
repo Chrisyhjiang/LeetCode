5ac2ece55682c2f5bfbab78030a3c81e644552bc
Christopher Yihan Jiang <chrisyh.jiang@gmail.com>
2022-08-18 10:03:49 -0500

    Create largestRectangleHistogram.java

---
+++ Array/largestRectangleHistogram.java
@@ -0,0 +1,30 @@
+// monotonic stack
+import java.util.*;
+public class largestRectangleHistogram {
+    public static void main(String[] args){
+        int[] arr = {2,1,5,6,2,3};
+        int res = largestRectangleArea(arr);
+        System.out.println(res);
+    }
+    public static int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1)
+                    && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+    }
+}
