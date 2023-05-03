e84883355ca297dae4b7009887ffc8fdcd928aa8
Christopher Yihan Jiang <chrisyh.jiang@gmail.com>
2023-05-03 08:21:36 -0500

    Create maxCount.java

---
+++ contest/maxCount.java
@@ -0,0 +1,23 @@
+
+public class maxCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int maximumCount(int[] nums) {
+		int pos = 0;
+		int neg = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] > 0) {
+				pos++;
+			}
+			if(nums[i] < 0) {
+				neg++;
+			}
+		}
+		return Math.max(pos, neg);
+	}
+
+}
